2b55c0c7417c8113fa57a5fdd43f396b9d7f209d
Calvin Vuong <44008458+OuyaSusumu@users.noreply.github.com>
2022-03-30 15:04:43 -0400

    Added code

--- src/main/java/com/demo/FizzBuzz.java
+++ src/main/java/com/demo/FizzBuzz.java
@@ -4,14 +4,24 @@ public class FizzBuzz
 {
     String processNumber(int num)
 	{	
-    	// Copy your code here
-		return "";
+    		if (num%3 == 0 && num%5 == 0){
+			return "FizzBuzz";
+		  } else if (num%3 == 0) {
+			return "Fizz";
+		  } else if (num%5 == 0) {
+			return "Buzz";
+		  } else
+			return ""+num;
 	}
 	
 	String processNumbers (int[] numList)
 	{
-    	// Copy your code here
-		return "";
-	}
+    		int length = numList.length;
+        	String result = "";
+        	for(int i = 0; i < length-1; i++){
+        	  result += (String) processNumber(numList[i]) + ", ";
+        	}
+        	return result + (String) processNumber(numList[length-1]);
+		}
 
 }
